94c358e6a9c98bc8713a485b680a1086b301523c
Noémie Koelblen <noemie.koelblen@icloud.com>
2024-01-22 10:16:35 +0100

    Lightbox structure

--- footer.php
+++ footer.php
@@ -8,6 +8,27 @@
 ?>
     </main><!-- #main -->
 
+	<div id="lightbox">
+		<button class="close-btn">
+			<img src="<?= get_template_directory_uri() . '/assets/images/lightbox-close.svg'; ?> " alt="fermer la visionneuse de photo">
+		</button>
+		<button class="nav-previous">
+			<img src="<?= get_template_directory_uri() . '/assets/images/lightbox-prev.svg'; ?> " alt="photo précédente">
+			<span>Précédente</span>
+		</button>
+		<div class="photo-container">
+			<div class="photo"></div>
+			<div class="informations">
+				<h2 class="title"></h2>
+				<p class="categorie"></p>
+			</div>
+		</div>
+		<button class="nav-next">
+			<span>Suivante</span>
+			<img src="<?= get_template_directory_uri() . '/assets/images/lightbox-next.svg'; ?> " alt="photo suivante">
+		</button>
+	</div>
+
 	<footer id="colophon" class="site-footer">
 			<nav class="footer-navigation">
 				<ul class="footer-navigation-wrapper">
