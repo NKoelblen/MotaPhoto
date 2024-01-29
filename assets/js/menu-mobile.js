d62a1b6db89d4f448ee9d1a14246d7edee082287
Noémie Koelblen <noemie.koelblen@icloud.com>
2024-01-29 09:54:15 +0100

    Menu Mobile Animation

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -2,22 +2,19 @@ const menuBtn = document.querySelector('#primary-mobile-menu');
 const openIcone = document.querySelector('.dropdown-icon.open');
 const closeIcone = document.querySelector('.dropdown-icon.close');
 const menuUL = document.querySelector('#primary-menu-list');
-function closemenu() {
-    menuBtn.setAttribute("aria-expanded", "false");
-    openIcone.style.setProperty('display', 'inline', 'important');
-    closeIcone.style.setProperty('display', 'none', 'important');
-    menuUL.style.setProperty('display', 'none', 'important');
-}
+
 menuBtn.addEventListener("click", () => {
     if (menuBtn.getAttribute( 'aria-expanded') === 'false' ) {
         menuBtn.setAttribute("aria-expanded", "true");
         openIcone.style.setProperty('display', 'none', "important");
         closeIcone.style.setProperty('display', 'inline', 'important');
-        menuUL.style.setProperty('display', 'flex', 'important');
+        menuUL.classList.remove("close");
+        menuUL.classList.add("open");
     } else {
         closemenu();
     }
 })
+
 document.querySelectorAll('#primary-menu-list li').forEach(link => {
     link.addEventListener("click", () => {
         if (window.matchMedia("(max-width: 430px)").matches) {
@@ -25,6 +22,15 @@ document.querySelectorAll('#primary-menu-list li').forEach(link => {
         }
     })
 })
+
+function closemenu() {
+    menuBtn.setAttribute("aria-expanded", "false");
+    openIcone.style.setProperty('display', 'inline', 'important');
+    closeIcone.style.setProperty('display', 'none', 'important');
+    menuUL.classList.remove("open");
+    menuUL.classList.add("close");
+}
+
 window.addEventListener("resize", () => {
     if (window.matchMedia("(min-width: 430px)").matches) {
         menuUL.style.setProperty('display', 'flex', 'important');
